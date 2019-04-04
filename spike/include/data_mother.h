5a614531d1dc1b57059f7f1593efebcb091d340e
jing-q-xu <jing.q.xu@hotmail.com>
2019-04-04 15:08:08 +0800

    refactor

---
+++ spike/include/data_mother.h
@@ -0,0 +1,37 @@
+#ifndef __DATA_MOTHER_H__
+#define __DATA_MOTHER_H__
+#include "actor.h"
+struct DummyActor : Actor
+{
+    DummyActor() : m_count(0)
+    {
+
+    }
+    int msg_received(const char *msg, int len)
+    {
+        if(len == sizeof(Greeting))
+        {
+            const Greeting* greet = (const Greeting*)msg;
+            int sequence = greet->count;
+            if(m_count != -1)
+            {
+                if(m_count + 1 != sequence)
+                {
+                    std::cout << "error, expect:" << m_count + 1 << " received:" << sequence << std::endl;
+                }
+            }
+            m_count = sequence;
+            if (m_count % 100 == 0)
+            {
+                std::cout << "client send count: " << m_count << std::endl;
+            }
+            }
+            else
+            {
+                std::cout << "error, unexpected length received:" << std::endl;
+            }
+        return 0;
+    }
+    int m_count;
+};
+#endif
--- spike/reqrep/server_demo.cpp
+++ spike/reqrep/server_demo.cpp
@@ -1,40 +1,14 @@
 #include "server.h"
 #include <iostream>
 #include "msg.h"
+#include "data_mother.h"
 
 namespace
 {
 int count = -1;
-struct DummyActor : Actor
-{
-    int msg_received(const char *msg, int len)
-    {
-        if(len == sizeof(Greeting))
-        {
-            const Greeting* greet = (const Greeting*)msg;
-            int sequence = greet->count;
-            if(count != -1)
-            {
-                if(count + 1 != sequence)
-                {
-                    std::cout << "error, expect:" << count + 1 << " received:" << sequence << std::endl;
-                }
-            }
-            count = sequence;
-            if (count % 100 == 0)
-            {
-                std::cout << "client send count: " << count << std::endl;
-            }
-            }
-            else
-            {
-                std::cout << "error, unexpected length received:" << std::endl;
-            }
-        return 0;
-    }
-}dummy;
-
+DummyActor dummy;
 }
+
 int main (const int argc, const char **argv)
 {
     const char *url = argv[1];
